48a2613941e8ff68d8948ddd109843019a171886
Mayank Agrawal <agarwalm214@gmail.com>
2016-10-04 12:11:40 +0530

    modify settings

--- hitchhike/settings.py
+++ hitchhike/settings.py
@@ -118,3 +118,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+
+
+import couchdb
+
+cloudant_data = {
+    'user' : 'c8f0887d-5214-4cd8-bb01-e7c0c8448c95-bluemix',
+    'password' : "115a155b01dae21ffde4810f396f4e29166bc8e1b2472f667917fb6299938dad",
+    'host' : 'c8f0887d-5214-4cd8-bb01-e7c0c8448c95-bluemix.cloudant.com',
+    'port' : '443'
+}
+server = couchdb.Server(
+            "https://"+cloudant_data['user']+':'+
+            cloudant_data['password']+'@'+cloudant_data['host']+':'+
+            cloudant_data['port']
+            )
+try:
+    db = server.create("hitchdata")
+except:
+    db = server["hitchdata"]
